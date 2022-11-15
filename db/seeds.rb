21738dfd1e38ca4b8b2fd3956abbeef81b6927b9
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-15 12:35:57 -0500

    created user seeds and deleted user.name column migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+User.destroy_all
+
+puts "Creating users..."
+hugo = { first_name: 'Hugo', last_name: 'Dulac', address: 'Hochelaga', email: 'hd@gmail.com', password: '123456' }
+jessica = { first_name: 'Jessica', last_name: 'Iacovozzi', address: 'Lasalle', email: 'ji@gmail.com', password: '123456' }
+giovanni = { first_name: 'Giovanni', last_name: 'Reid', address: 'Rockland', email: 'gr@gmail.com', password: '123456' }
+eva = { first_name: 'Eva', last_name: 'Roux', address: 'Plateau', email: 'er@gmail.com', password: '123456' }
+
+[hugo, jessica, giovanni, eva].each do |attributes|
+  user = User.create!(attributes)
+  puts "Created #{user.name}"
+end
+puts "Finished!"
