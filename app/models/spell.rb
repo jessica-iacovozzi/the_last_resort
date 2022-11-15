0721ce1fdbbd24d57ea4dbf188818c355a2d2cdb
Hugo Dulac <hugodulaclemelin@gmail.com>
2022-11-15 14:18:27 -0500

    Spell model index and seeds done

--- app/models/spell.rb
+++ app/models/spell.rb
@@ -1,3 +1,10 @@
 class Spell < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :location, presence: true
+  validates :price, presence: true
+  validates :user, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,43 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #puts "Cleaning database..."
-Spell.destroy_all
-
-puts "Creating spells..."
-love_bond = { name: "Love Bond",
-              user_id: User.all.sample,
-              description: "Make someone fall in love with you",
-              location: "Villeray", rating: "5",
-              category: "Love",
-              price: "95" }
-cook_well = { name: "Cook Well", description: "Bring out the best taste in you dish, everytime", location: "Plateau",
-              rating: "4", price: "65",
-              category: "Home" }
-disable_stalker = { name: "Disable Stalker",
-                    description: "Any stalker who starts following you will suddenly get injured",
-                    location: "Montreal", rating: "4", price: "110", category: "Social" }
-block_mother_in_law = { name: "Disable Stalker",
-                        description: "Any call from your mother in law will be mysteriously  redirected",
-                        location: "Village",
-                        category: "Family",
-                        rating: "5",
-                        price: "45" }
-always_clean = { name: "Always Clean",
-                 description: "Every rooms in your home clean themselves",
-                 location: "Mile-End",
-                 category: "Home",
-                 rating: "5",
-                 price: "45" }
-[love_bond, cook_well, disable_stalker, block_mother_in_law, always_clean].each do |attributes|
-  spell = Spell.create!(attributes)
-  puts "Created #{spell.name}"
-end
-puts "Finished!"
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
 puts "Cleaning database..."
 User.destroy_all
 
@@ -52,3 +15,42 @@ eva = { first_name: 'Eva', last_name: 'Roux', address: 'Plateau', email: 'er@gma
   puts "Created #{user.first_name}"
 end
 puts "Done!"
+
+Spell.destroy_all
+
+puts "Creating spells..."
+love_bond = { name: "Love Bond",
+              user: User.all.sample,
+              description: "Make someone fall in love with you",
+              location: "Villeray",
+              category: "Love",
+              price: "95" }
+cook_well = { name: "Cook Well", user: User.all.sample,
+              description: "Bring out the best taste in you dish, everytime", location: "Plateau",
+              price: "65",
+              category: "Home" }
+disable_stalker = { name: "Disable Stalker",
+                    user: User.all.sample,
+                    description: "Any stalker who starts following you will suddenly get injured",
+                    location: "Montreal", price: "110", category: "Social" }
+block_mother_in_law = { name: "Block Mother In Law",
+                        user: User.all.sample,
+                        description: "Any call from your mother in law will be mysteriously  redirected",
+                        location: "Village",
+                        category: "Family",
+                        price: "45" }
+always_clean = { name: "Always Clean",
+                 user: User.all.sample,
+                 description: "Every rooms in your home clean themselves",
+                 location: "Mile-End",
+                 category: "Home",
+                 price: "45" }
+[love_bond, cook_well, disable_stalker, block_mother_in_law, always_clean].each do |attributes|
+  spell = Spell.create!(attributes)
+  puts "Created #{spell.name}"
+end
+puts "Finished!"
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
