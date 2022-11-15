5ad4af02d746303d5a12e71ec3ae41d1a0d92534
jessica iacovozzi <iacovozzi.jessica@gmail.com>
2022-11-15 11:50:13 -0500

    fixed CSRF token authenticity error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  # fixes "Can't verify CSRF token authenticity" error
+  protect_from_forgery with: :null_session,
+      if: Proc.new { |c| c.request.format =~ %r{application/json} }
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address, :email, :password])
