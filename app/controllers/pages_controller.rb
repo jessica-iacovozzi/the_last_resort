ed61016772b651589d81defcca29e573d41c599b
Giovanni Reid <giovanni-mtl@hotmail.com>
2022-11-16 11:51:55 -0500

    created the cards section adn added dynamic images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,11 @@ class PagesController < ApplicationController
       'https://res.cloudinary.com/dppymdnxh/image/upload/v1668617000/spell%20images/2306148a6826c3849554ff6e82f358c0_tm8orh.jpg',
       'https://res.cloudinary.com/dppymdnxh/image/upload/v1668616976/spell%20images/bdf4e8e60c1fedae47811e60754532c1_hnmf8b.png'
     ]
-    @user_img = ['']
+    @user_img = [
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668617351/spell%20images/users/09-angela_wj9jud.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668617350/spell%20images/users/01-dimitri_gjcpcp.jpg', 'https://res.cloudinary.com/dppymdnxh/image/upload/v1668617350/spell%20images/users/02-tanya_ilefnh.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668617350/spell%20images/users/08-patt_rffvlp.jpg',
+      'https://res.cloudinary.com/dppymdnxh/image/upload/v1668617350/spell%20images/users/03-steve_uaz0jx.jpg'
+    ]
   end
 end
