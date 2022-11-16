ccbf19217457984f667d254bb9f674bec368f200
Eva Roux <114696769+Evarouux@users.noreply.github.com>
2022-11-16 13:57:39 -0500

    create and new methods in bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,11 +8,26 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+
+    if @booking.save
+      redirect_to my_bookings_path, notice: "Your booking is created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def my_bookings
+    @bookings = Booking.all
   end
 
   def delete
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:status, :start_date, :end_date, :spell_id)
   end
 end
