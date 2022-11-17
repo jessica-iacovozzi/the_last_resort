043fef09060e3df724ce7d816326102091e2a358
Eva Roux <114696769+Evarouux@users.noreply.github.com>
2022-11-17 14:05:40 -0500

    fixing bug geocoder

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,5 +7,8 @@ import { application } from "./application"
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
+import LocationAutocompleteController from "./location_autocomplete_controller"
+application.register("location-autocomplete", LocationAutocompleteController)
+
 import MapController from "./map_controller"
 application.register("map", MapController)
