0fc3137258f47dfefd76e75e99cbcbf727fdc1c0
Eva Roux <114696769+Evarouux@users.noreply.github.com>
2022-11-18 13:12:29 -0500

    fixing geocoder and adding css style My request and Bookings

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -13,7 +13,7 @@ export default class extends Controller {
 
 		this.map = new mapboxgl.Map({
 			container: this.element,
-			style: "mapbox://styles/mapbox/streets-v10",
+			style: "mapbox://styles/evaroux/clamqab2l000215mnzwgeggsj",
 		});
 
 		this.#addMarkersToMap();
